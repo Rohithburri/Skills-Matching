09f730758b805eae9584dca73344563a6192d49e
Rohithburri <153207737+Rohithburri@users.noreply.github.com>
2025-05-17 17:30:27 +0530

    Add files via upload

---
+++ SmartJobRecommendationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.smart.job.recommendation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartJobRecommendationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
